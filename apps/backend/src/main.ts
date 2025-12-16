f7f20bc8dee48a6e91d17ee74d8d1a2b3d6760e2
Jonathan Borges <mborges.jonathan@gmail.com>
2025-12-16 17:47:03 -0300

    feat: install deps nestjs + prism and add schema for tables

---
+++ apps/backend/src/main.ts
@@ -0,0 +1,20 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   await app.listen(process.env.PORT ?? 3000);
+// }
+// bootstrap();
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  app.setGlobalPrefix('api');
+  app.enableCors();
+  await app.listen(3000);
+}
+bootstrap();
